fc8002d13ec0d2ec535097a3db8c40853ffab387
Nurzat <nurzat.kaz@gmail.com>
2019-10-21 23:01:32 +0600

    db changes

--- config/db.php
+++ config/db.php
@@ -7,6 +7,13 @@ return [
     'password' => '',
     'charset' => 'utf8',
 
+
+//    'class' => 'yii\db\Connection',
+//    'dsn' => 'mysql:host=srv-db-plesk02.ps.kz:3306;dbname=ibeacon1_ecos',
+//    'username' => 'ibeac_ecos',
+//    'password' => '1q2w3e4r',
+//    'charset' => 'utf8',
+
     // Schema cache options (for production environment)
     //'enableSchemaCache' => true,
     //'schemaCacheDuration' => 60,
